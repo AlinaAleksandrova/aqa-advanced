2caf34a3cbf105613a3f330618d4d89291473bfd
Alina <aleksandrovaalina4@gmail.com>
2025-03-27 14:34:13 -0500

    added an example of using chalk in nodeBasics/index.js

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello! I just vikorist chalk for the color text.'));
+console.log(chalk.green('This text is green!'));
+console.log(chalk.red.bold('And this one is red and fat!'));
